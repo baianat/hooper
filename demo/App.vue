cae8caf409762f9497dd2bf761abe22f6d7b10a1
Abdelrahman Ismail <abdelrahman3d@gmail.com>
2018-12-09 13:09:21 +0300

    feat: use svg icon for control carousel

--- demo/App.vue
+++ demo/App.vue
@@ -23,8 +23,14 @@
 
 
     <!-- optionaly elements -->
-    <span slot="hooper-next">>></span>
-    <span slot="hooper-prev"><<</span>
+    <svg height="24" viewBox="0 0 24 24" class="icon-keyboard-arrow-right" slot="hooper-next">
+      <title>keyboard arrow right</title>
+      <path d="M8.29,17.29,13.59,12,8.29,6.71A1,1,0,0,1,9.71,5.29l6,6a1,1,0,0,1,0,1.42l-6,6a1,1,0,0,1-1.42,0A1,1,0,0,1,8.29,17.29Z" />
+    </svg>
+    <svg width="24" height="24" viewBox="0 0 20 20" class="icon-keyboard-arrow-left" slot="hooper-prev">
+      <path d="M15.71,6.71,10.41,12l5.3,5.29a1,1,0,0,1,0,1.42,1,1,0,0,1-1.42,0l-6-6a1,1,0,0,1,0-1.42l6-6a1,1,0,1,1,1.42,1.42Z" />
+    </svg>
+
   </hooper>
 </div>
 
