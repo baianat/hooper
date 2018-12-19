c489c6f3d578fd8e293913c1d59bfa859dd76356
Abdelrahman Ismail <abdelrahman3d@gmail.com>
2018-12-19 15:00:48 +0300

    chore: add deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,15 +3,6 @@
 # abort on errors
 set -e
 
-# build
-npm run docs:build
-
-# navigate into the build output directory
-cd docs/.vuepress/dist
-
-git init
-git add -A
-git commit -m 'deploy'
-
-git push -f git@github.com:baianat/hooper.git master:gh-pages
-cd -
+npm version patch
+npm run build
+npm publish
