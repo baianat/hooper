bef56df2c3f0c9f1d46bd2055c29e84089977118
Abdelrahman Ismail <abdelrahman3d@gmail.com>
2018-12-19 14:58:00 +0300

    feat: icons component is now available to use

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import Hooper from './Hooper.vue';
 import Slide from './Slide.vue';
+import Icons from './addons/Icons';
 import Progress from './addons/Progress.vue';
 import Pagination from './addons/Pagination.vue';
 import Navigation from './addons/Navigation.vue';
 
-export { Hooper, Slide, Progress, Pagination, Navigation };
+export { Hooper, Slide, Progress, Pagination, Navigation, Icons };
 export default Hooper;
