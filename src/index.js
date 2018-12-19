df78611da3408ca66f685e72f1d29517a9889542
Abdelrahman Awad <logaretm1@gmail.com>
2018-12-19 18:26:17 +0200

    feat: added addonMixin to abstract Provide/Inject API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Hooper from './Hooper.vue';
 import Slide from './Slide.vue';
+import addonMixin from './addons/Mixin';
 import Icons from './addons/Icons';
 import Progress from './addons/Progress.vue';
 import Pagination from './addons/Pagination.vue';
 import Navigation from './addons/Navigation.vue';
 
-export { Hooper, Slide, Progress, Pagination, Navigation, Icons };
+export { Hooper, Slide, Progress, Pagination, Navigation, Icons, addonMixin };
+
 export default Hooper;
