c699a21c68cd9a800f0db0e34fc109e0c0cfe488
Abdelrahman Ismail <abdelrahman3d@gmail.com>
2019-01-31 17:05:11 +0300

    chore: update build scripts

--- scripts/build.js
+++ scripts/build.js
@@ -6,32 +6,27 @@ const chalk = require('chalk');
 const mkdirpNode = require('mkdirp');
 const { rollup } = require('rollup');
 const { promisify } = require('util');
-const { builds, paths } = require('./config');
+const { configs, paths, common } = require('./config');
 
 const mkdirp = promisify(mkdirpNode);
 
 build('umd');
-build('es');
+build('esm');
 
-async function build (format) {
+async function build (build) {
   await mkdirp(paths.dist);
-  console.log(chalk.cyan(`Generating ${format} build...`));
-
-  const bundle = await rollup(builds.input);
-  const { output } = await bundle.generate({
-    format,
-    ...builds.output
-  });
-
+  console.log(chalk.cyan(`Generating ${build} build...`));
+  const bundle = await rollup(configs[build].input);
+  const { output } = await bundle.generate(configs[build].output);
   const code = output[0].code;
-  let extensions = format === 'es' ? '.esm' : '';
-  const outputPath = path.join(paths.dist, `hooper${extensions}.js`);
+  
+  const outputPath = path.join(paths.dist, `${common.name}${configs[build].ext}.js`);
   fs.writeFile(outputPath, code, (err) => {
     if (err) {
       throw err;
     }
     let stats = getStats({ code, path: outputPath });
-    console.log(`${chalk.green(`Output File: hooper ${format}`).padEnd(35, ' ')} ${stats}`);
+    console.log(`${chalk.green(`Output File: hooper ${build}`).padEnd(35, ' ')} ${stats}`);
   });
 }
 
--- scripts/config.js
+++ scripts/config.js
@@ -3,44 +3,85 @@ const resolve = require('rollup-plugin-node-resolve');
 const babel = require('rollup-plugin-babel');
 const replace = require('rollup-plugin-replace');
 const css = require('rollup-plugin-css-only');
-const vue = require('rollup-plugin-vue').default;
+const vue = require('rollup-plugin-vue');
+const commonjs = require('rollup-plugin-commonjs');
 const version = process.env.VERSION || require('../package.json').version;
 
 const paths = {
   src: path.join(__dirname, '../src'),
-  dist: path.join(__dirname, '../dist')
+  dist: path.join(__dirname, '../dist'),
+  outputFolder: path.join(__dirname, '../dist'),
+}
+
+const common = {
+  name: 'hooper',
+  input: path.join(paths.src, 'index.js'),
+  uglifyOptions: {
+    toplevel: true,
+    compress: true,
+    mangle: true
+  },
+  banner:
+    `/**
+  * Hopper ${version}
+  * (c) ${new Date().getFullYear()}
+    * @license MIT
+    */`,
+  plugins: [
+    replace({ __VERSION__: version }),
+    css({ output: 'dist/hooper.css' }),
+    commonjs(),
+    vue({ css: false }),
+    babel(),
+    resolve()
+  ]
 }
 const builds = {
-  input: {
-    input: path.join(paths.src, 'index.js'),
-    plugins: [
-      replace({ __VERSION__: version }),
-      css({ output: 'dist/hooper.css' }),
-      vue({ css: false }),
-      babel(),
-      resolve()
-    ]
+  umd: {
+    format: 'umd',
+    name: 'Hooper',
+    ext: ''
   },
-  output: {
-    name: 'hooper',
-    banner:
-      `/**
-    * Hopper ${version}
-    * (c) ${new Date().getFullYear()}
-      * @license MIT
-      */`,
-      outputFolder: path.join(__dirname, '../dist'),
-  }
-};
-
-const uglifyOptions = {
-  toplevel: true,
-  compress: true,
-  mangle: true
+  esm: {
+    format: 'es',
+    ext: '.esm'
+  },
+  old: {
+    format: 'umd',
+    name: 'Hooper',
+    ext: '.old'
+  },
+  oldEsm: {
+    format: 'es',
+    ext: '.old.esm'
+  },
 }
+
+function getConfig (key) {
+  const build = builds[key];
+  const config = {
+    ...build,
+    input: {
+      input: build.input || common.input,
+      plugins: build.plugins || common.plugins,
+    },
+    output: {
+      name: build.name || common.name,
+      banner: common.banner,
+      format: build.format,
+      exports: 'named'
+    }
+  }
+  return config;
+}
+
+const configs = Object.keys(builds).reduce((acc, build) => {
+  acc[build] = getConfig(build);
+  return acc;
+}, {})
 
 module.exports = {
   paths,
-  builds,
-  uglifyOptions
+  configs,
+  common
 };
