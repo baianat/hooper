26dda3e331b77d7d6576c2ca700a7ba784efaf91
Abdelrahman Awad <logaretm1@Gmail.com>
2019-06-26 16:40:36 +0200

    fix #103 (#105)
    * fix: polyfill Math.sign and Object.assign closes #103
    
    * fix: add polyfill in docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,9 +5,10 @@ module.exports = {
   serviceWorker: true,
   head: [
     ['meta', { charset: 'utf-8' }],
-    ['meta', { name: "theme-color", content: "#41b883" }],
+    ['meta', { name: 'theme-color', content: '#41b883' }],
     ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
     ['meta', { property: 'og:image', content: '/hooper/hooper.png' }],
+    ['script', { src: 'https://polyfill.io/v3/polyfill.min.js', crossorigin: 'anonymous' }]
   ],
   themeConfig: {
     repo: 'baianat/hooper',
@@ -20,16 +21,9 @@ module.exports = {
         label: 'English',
         selectText: 'Languages',
         editLinkText: 'Help us improve this page!',
-        nav: [
-          { text: 'API', link: '/api' },
-          { text: 'Examples', link: '/examples' }
-        ],
-        sidebar: [
-          '/getting-started',
-          '/examples',
-          '/api'
-        ]
+        nav: [{ text: 'API', link: '/api' }, { text: 'Examples', link: '/examples' }],
+        sidebar: ['/getting-started', '/examples', '/api']
       }
     }
   }
-}
+};
--- src/utils.js
+++ src/utils.js
@@ -79,3 +79,42 @@ export function cloneSlide(vnode, indx) {
 
   return h(tag, data, children);
 }
+
+// IE11 :)
+function assignPoly(target) {
+  if (target === undefined || target === null) {
+    throw new TypeError('Cannot convert first argument to object');
+  }
+
+  var to = Object(target);
+  for (var i = 1; i < arguments.length; i++) {
+    var nextSource = arguments[i];
+    if (nextSource === undefined || nextSource === null) {
+      continue;
+    }
+    nextSource = Object(nextSource);
+
+    var keysArray = Object.keys(Object(nextSource));
+    for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+      var nextKey = keysArray[nextIndex];
+      var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+      if (desc !== undefined && desc.enumerable) {
+        to[nextKey] = nextSource[nextKey];
+      }
+    }
+  }
+
+  return to;
+}
+
+export const assign = Object.assign || assignPoly;
+
+function signPoly(value) {
+  if (value < 0) {
+    return -1;
+  }
+
+  return value > 0 ? 1 : 0;
+}
+
+export const sign = Math.sign || signPoly;
