44a88ef1c23699b3373c4e5cf9e9f353544cfccd
Abdelrahman Awad <logaretm1@gmail.com>
2019-06-26 12:16:48 +0200

    refactor: pagination component as a render fn

--- src/utils.js
+++ src/utils.js
@@ -118,3 +118,11 @@ function signPoly(value) {
 }
 
 export const sign = Math.sign || signPoly;
+
+export function normalizeChildren(context, slotProps = {}) {
+  if (context.$scopedSlots.default) {
+    return context.$scopedSlots.default(slotProps) || [];
+  }
+
+  return context.$slots.default || [];
+}
